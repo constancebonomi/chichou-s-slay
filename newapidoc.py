4cf33612431a772d1ed838512d1feb16d2e267a0
jeremiswierkosz <164135972+jeremiswierkosz@users.noreply.github.com>
2024-04-01 20:01:15 +0200

    Update newapidoc.py

--- newapidoc.py
+++ newapidoc.py
@@ -1,1 +1,8 @@
+import requests as rq
+origin = input("Where are you now? ")
+destination = input("Where are you going? ")
+address = "http://transport.opendata.ch/v1/connections?from=" + origin + "&to=" + destination
+print(address)
+r = (rq.get(address)).json()
+deptplatform = r["connections"][0]['from']['station']
 
