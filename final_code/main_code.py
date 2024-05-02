7cca0d16b9c67af6d669c02f188c9edd44571e0d
jeremiswierkosz <164135972+jeremiswierkosz@users.noreply.github.com>
2024-05-02 21:11:14 +0200

    Update main_code.py

--- final_code/main_code.py
+++ final_code/main_code.py
@@ -4,7 +4,7 @@ import json
 from PIL import Image
 import io
 
-recipedict_withingredients = dict(open("recipe", "r"))
+recipedict_withingredients = dict(open("https://raw.githubusercontent.com/constancebonomi/chichou-s-slay/main/final_code/recipedict_withingredients.json", "r"))
 
 def recipesearch (chosen_ingredients_list = [], recipedict_withingredients = {}):
     
